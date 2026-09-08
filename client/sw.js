e53754f3b211d7ac5ddbd1d7ea963ba8f9e28e36
TechGeekPH <137922253+TechGeek-PH@users.noreply.github.com>
2026-09-08 19:32:55 +0800

    Force Client App to refresh latest chat assets

--- client/sw.js
+++ client/sw.js
@@ -1,5 +1,5 @@
-const CACHE='techgeekph-client-v7';
-const STATIC=['./manifest.webmanifest?v=20260903-6','../assets/TechGeekPH%20-%20logo.png'];
+const CACHE='techgeekph-client-v8';
+const STATIC=['./manifest.webmanifest?v=20260908-chatphoto1','../assets/TechGeekPH%20-%20logo.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
