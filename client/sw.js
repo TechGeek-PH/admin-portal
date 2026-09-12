7e54f6f457bdffb4839a70393ff767e71e2f32c2
TechGeekPH <137922253+TechGeek-PH@users.noreply.github.com>
2026-09-12 04:42:09 +0000

    Open Client Payment Center on same-origin /payment route

--- client/sw.js
+++ client/sw.js
@@ -1,4 +1,4 @@
-const CACHE='techgeekph-client-v8';
+const CACHE='techgeekph-client-v9-payment';
 const STATIC=['./manifest.webmanifest?v=20260908-chatphoto1','../assets/TechGeekPH%20-%20logo.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
